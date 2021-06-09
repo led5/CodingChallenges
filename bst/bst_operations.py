08ea1695bfddba7813cf6d37979ae9daa4b50d85
Dalena Le <49535854+led5@users.noreply.github.com>
2021-06-08 19:52:09 -0700

    Added insert_node, delete_node, find_node, inorder_traversal, preorder_traversal, postorder_traversal

--- bst/bst_operations.py
+++ bst/bst_operations.py
@@ -1,27 +1,122 @@
-'''
-A Binary Search Tree class demonstrating its basic operations. 
-
-'''
-
 class Node:
-    def __init__(self, data):
-        self.data = data 
+    def __init__(self, val):
+        self.data = val  
         self.left = None
         self.right = None 
-    
-    def create():
-        pass
-    def insert_node():
-        pass
-    def delete_node(): 
-        pass
-    def find_node():
-        pass
-    def inorder_traversal():
-        pass
-    def preorder_traversal():
-        pass
-    def postorder_traversal():
-        pass
-    def levelorder_traversal():
-        pass 
+
+    def insert_node(self, root, val):
+        '''
+            Given a value, insert a new node into the tree. 
+
+            root type: Node 
+            val type: int
+            rtype: Node 
+
+        '''
+        if not root: 
+            return Node(val)
+        else:
+            if root.data == val:
+                return root
+            elif root.data < val:
+                root.right = self.insert_node(root.right, val)
+            else:
+                root.left = self.insert_node(root.left, val)
+        return root 
+                
+    def delete_node(self, root, val):
+        '''
+            Given a value, delete the node with the given value.
+
+            root type: Node 
+            val type: int
+            rtype: None
+
+        '''
+        if not root:
+            return None
+        if val != root.data:
+            if val < root.data:
+                root = self.delete_node(root.left, val)
+            elif val > root.data:
+                root = self.delete_node(root.right, val)
+            return root 
+        else:
+            if not root.left:
+                return root.right
+            if not root.right:
+                return root.left
+
+            r_subtree = root.right
+            while(r_subtree.left):
+                r_subtree = r_subtree.left 
+            r_subtree.left = root.left 
+            return root.right
+        return self.delete_node(root, val)
+
+    def find_node(self,root, val):
+        '''
+            Find the given value of the BST. 
+            
+            root type: Node 
+            val type: int
+            rtype: None 
+
+        '''
+        while(root):
+            if val < root.data:
+                root = root.left
+            elif val > root.data:
+                root = root.data
+            else:
+                return root
+        return None 
+
+    def inorder_traversal(self, root):
+        '''
+            Print the BST inorder. 
+
+            root type: Node 
+            rtype: None
+        '''
+        if not root:
+            return None
+        print(root.left)
+        print(root.data)
+        print(root.right)
+
+    def preorder_traversal(self, root):
+        '''
+            Print the BST preorder. 
+
+            root type: Node 
+            rtype: None 
+        '''
+        if not root:
+            return None
+        print(root.data)
+        print(root.left)
+        print(root.right)
+
+    def postorder_traversal(self, root):
+        '''
+            Print the BST postorder. 
+
+            root type: Node
+            rtype: None
+        '''
+        if not root:
+            return None
+        print(root.left)
+        print(root.right)
+        print(root.data)
+
+    def levelorder_traversal(self, root):
+
+
+if __name__ == "__main__":
+    root = Node(6)
+    root = root.insert_node(root, 1)
+    root = root.insert_node(root, 10)
+    root = root.insert_node(root, 23)
+    root = root.delete_node(root, 23)
