b6a36a274e8816cddac4f2a9d2f2ad1fdab612e6
Dalena Le <49535854+led5@users.noreply.github.com>
2021-06-08 20:39:43 -0700

    added levelorder_traversal, get_height, print_levelorder

--- bst/bst_operations.py
+++ bst/bst_operations.py
@@ -112,13 +112,40 @@ class Node:
         print(root.data)
 
     def levelorder_traversal(self, root):
-        pass 
+        height = self.get_height(root)
+        for i in range(1, height+1):
+            self.print_levelorder(root, i)
+        
+    def get_height(self, root):
+        if not root: 
+            return 0
+        return max(self.get_height(root.right), self.get_height(root.left)) + 1
+    
+    def print_levelorder(self, root, level):
+        if not root:
+            return None
+        if level == 1:
+            print(root.data)
+        elif level > 1:
+            self.print_levelorder(root.left, level-1)
+            self.print_levelorder(root.right, level-1)
+
 
 
 if __name__ == "__main__":
-    
+
     root = Node(6)
     root = root.insert_node(root, 1)
     root = root.insert_node(root, 10)
     root = root.insert_node(root, 23)
+
     root = root.delete_node(root, 23)
+
+    root = root.insert_node(root, 35)
+
+    root = root.inorder_traversal(root)
+    root = root.preorder_traversal(root)
+    root = root.postorder_traversal(root)
+    root = root.levelorder_traversal(root)
+
+
