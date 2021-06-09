ce16f8e939e03f09dd2b80a748d7e70f1e7907d6
Dalena Le <49535854+led5@users.noreply.github.com>
2021-06-08 17:06:37 -0700

    Edited transpose solution and added bst with class skeleton

---
+++ bst/bst_operations.py
@@ -0,0 +1,27 @@
+'''
+A Binary Search Tree class demonstrating its basic operations. 
+
+'''
+
+class Node:
+    def __init__(self, data):
+        self.data = data 
+        self.left = None
+        self.right = None 
+    
+    def create():
+        pass
+    def insert_node():
+        pass
+    def delete_node(): 
+        pass
+    def find_node():
+        pass
+    def inorder_traversal():
+        pass
+    def preorder_traversal():
+        pass
+    def postorder_traversal():
+        pass
+    def levelorder_traversal():
+        pass 
